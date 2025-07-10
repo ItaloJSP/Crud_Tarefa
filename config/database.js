38dfeb8c13c9cac5421b59bae034780648c1db04
italo_jose <italo3720523@faculdadecci.com.br>
2025-07-10 12:34:59 -0300

    fix: ajusta conexão DB com variáveis Railway

--- config/database.js
+++ config/database.js
@@ -1,8 +1,21 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('CRUD', 'postgres', 'Admin', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,    // nome do banco
+  process.env.DB_USER,    // usuário
+  process.env.DB_PASSWORD, // senha
+  {
+    host: process.env.DB_HOST, // host do banco
+    port: process.env.DB_PORT, // porta (opcional, mas recomendado)
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 module.exports = sequelize;
