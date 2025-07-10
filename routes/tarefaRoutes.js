df82dcd11ec1a54371b749216a2c5677ca41d4f4
italo_jose <italo3720523@faculdadecci.com.br>
2025-07-10 17:18:46 -0300

    SEGURANCA

--- routes/tarefaRoutes.js
+++ routes/tarefaRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const tarefaController = require('../controllers/TarefaController');
-const authMiddleware = require('../middlewares/auth');
+const TarefaController = require('../controllers/TarefaController');
+const authMiddleware = require('../middleware/auth');
 
-router.post('/tarefas', authMiddleware, tarefaController.criarTarefa);
-router.get('/tarefas', authMiddleware, tarefaController.listarTarefas);
-router.put('/tarefas/:id', authMiddleware, tarefaController.atualizarTarefa);
-router.delete('/tarefas/:id', authMiddleware, tarefaController.deletarTarefa);
+router.post('/tarefas', authMiddleware, TarefaController.criarTarefa);
+router.get('/tarefas', authMiddleware, TarefaController.listarTarefas);
+router.put('/tarefas/:id', authMiddleware, TarefaController.atualizarTarefa);
+router.delete('/tarefas/:id', authMiddleware, TarefaController.deletarTarefa);
 
 module.exports = router;
