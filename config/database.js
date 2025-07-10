171be6f0c216559af6047522164c456a7125c5f3
italo_jose <italo3720523@faculdadecci.com.br>
2025-07-10 12:41:56 -0300

    fix: adiciona e corrige config/database.js

--- config/database.js
+++ config/database.js
@@ -1,20 +1,14 @@
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,    // nome do banco
-  process.env.DB_USER,    // usuário
-  process.env.DB_PASSWORD, // senha
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST, // host do banco
-    port: process.env.DB_PORT, // porta (opcional, mas recomendado)
+    host: process.env.DB_HOST,
     dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    port: 5432,
+    logging: false,
   }
 );
 
