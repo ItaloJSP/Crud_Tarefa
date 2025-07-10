c47ceb4004e87d1e6cda6414022eda4badedeec0
italo_jose <italo3720523@faculdadecci.com.br>
2025-07-10 14:28:42 -0300

    fix: POSTGRESQL

--- config/database.js
+++ config/database.js
@@ -1,15 +1,16 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.URL_DO_BANCO_DE_DADOS, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
+const sequelize = new Sequelize(
+  'postgresql://postgres:ncmmkbVbmunJTxQKrFsonENLYdVDPDWs@switchyard.proxy.rlwy.net:57248/railway',
+  {
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // necessário no Railway
+      },
     },
-  },
-  logging: false,
-});
+  }
+);
 
 module.exports = sequelize;
